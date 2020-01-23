35be64dba59690f696f81d923e753c7b78d471d1
lapinskijw <jlapinski.dev@gmail.com>
2020-01-23 18:02:56 -0500

    created project and removed storyboards

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DailyTemperatureViewController.swift
+//  WeatherApp
+//
+//  Created by James Lapinski on 1/23/20.
+//  Copyright © 2020 JWL. All rights reserved.
+//
+
+import UIKit
+
+class DailyTemperatureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
